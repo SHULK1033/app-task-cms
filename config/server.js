4f01539ee8d15c0cbf5e6a8786acae0bf38c553a
Andres Arias <Andres.Arias@inoutdelivery.com>
2021-03-09 15:22:19 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'faf78d4b9ec83241821ddf275634f845'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59400173-27ba-42b5-b9b4-c25fe92d9df2'
+};
